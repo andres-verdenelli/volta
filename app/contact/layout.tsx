083070d8c4b5fcef9ec38ceaa4562ca25a4871b6
Andres Verdenelli <andresverdenelli@icloud.com>
2026-03-21 11:28:08 -0300

    Delete unused files

--- app/contact/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function test({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="border-3 border-purple-700 p-4">
-      <h1>Soy el layout wraper</h1>
-      {children}
-    </main>
-  )
-}
--- app/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="border border-blue-500">
-      <h1>Soy la pagina</h1>
-    </div>
-  )
-}
